30b725f492be33e472b85ec5d69bd318536e6350
Marco Robles <129124170+cyberlobo03@users.noreply.github.com>
2023-11-23 22:14:56 -0300

    Update ObraSocial.java

---
+++ src/main/java/com/salud/equipoT/entidad/ObraSocial.java
@@ -0,0 +1,18 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+
+public class ObraSocial(){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+}
